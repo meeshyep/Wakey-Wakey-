dc0df0d24d945bf87c1f6bc4ad48ac18bd0c7bd0
Tom Page <tom.w.page@gmail.com>
2016-11-22 15:10:29 +0000

    new passing test

--- clock/__tests__/index.ios.js
+++ clock/__tests__/index.ios.js
@@ -1,9 +1,11 @@
-import 'react-native';
+import React from 'react-native';
 import React from 'react';
 import Index from '../index.ios.js';
+import App from "./app/App";
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import moment from "moment";
 
 it('renders correctly', () => {
   const tree = renderer.create(
@@ -11,9 +13,30 @@ it('renders correctly', () => {
   );
 });
 
-
-
 test('expect the time to display with the correct time', () => {
-  const app = require('../app/App');
-  expect(App.state.time).toEqual(moment().format("LTS");
+  const app =
+    time: moment().format("LTS"),
+    date: moment().format("LL")
+    };
+
+  expect(app.time).toEqual(moment().format("LTS"));
+  expect(app.date).toEqual(moment().format("LL"));
   });
+
+
+
+
+
+
+
+
+test('expect the initial date in date picker to be current date', () => {
+   defaultProps = {
+    date: new Date(),
+  };
+  state = {
+    date: this.props.date
+  };
+
+  expect(state.date).toEqual(moment().format("LL"));
+});
