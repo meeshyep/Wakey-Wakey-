d29198fec99bd6841914362b0bc7ed50322952fc
Tom Page <tom.w.page@gmail.com>
2016-11-24 17:35:44 +0000

    escaping trouble

--- android/app/src/main/java/com/clock/MainApplication.java
+++ android/app/src/main/java/com/clock/MainApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.util.Log;
 
 import com.facebook.react.ReactApplication;
+import com.corbt.keepawake.KCKeepAwakePackage;
+import com.sh3rawi.RNAudioPlayer.RNAudioPlayer;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new KCKeepAwakePackage(),
+            new RNAudioPlayer(),
             new RCTCameraPackage()
       );
     }
